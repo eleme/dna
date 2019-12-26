d0f7bc5412dd76043fc65ea210427ce33e0b9ccf
yongguang.lyg <yongguang.lyg@alibaba-inc.com>
2019-12-26 19:26:56 +0800

    [CHG] init

---
+++ ios/Classes/DnaPlugin.m
@@ -0,0 +1,20 @@
+#import "DnaPlugin.h"
+
+@implementation DnaPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"dna"
+            binaryMessenger:[registrar messenger]];
+  DnaPlugin* instance = [[DnaPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
---
+++ test/dna_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:dna/dna.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('dna');
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await Dna.platformVersion, '42');
+  });
+}
