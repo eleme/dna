e18fdf74253c1de3c4def71e092f7f32cde2988a
yongguang.lyg <yongguang.lyg@alibaba-inc.com>
2020-01-16 16:41:03 +0800

    [CHG] 更新readme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,21 +25,13 @@ class _MyAppState extends State<MyApp> {
 
     try {
       platformVersion = await Dna.traversingNative((ObjCContext context) {
-        NativeObject version = context
-            .classFromString('UIDevice')
-            .invoke(method: 'currentDevice')
-            .invoke(method: 'systemVersion');
-        version.invoke(method: 'stringByAppendingString:', args: ['-iOS']);
+        NativeObject version = context.classFromString('UIDevice').invoke(method: 'currentDevice').invoke(method: 'systemVersion');
+        version = context.classFromString("NSString").invoke(method: 'stringWithString:', args: ['iOS-']).invoke(method: 'stringByAppendingString:', args: [version]);
+        context.returnVar = version; // 该句可省略
       }, (JAVAContext context) {
-        NativeObject versionId = context
-            .newJavaObjectFromConstructor(
-                'com.example.dna_example.DnaTest', null)
-            .invoke(method: 'getDnaVersion')
-            .invoke(method: 'getVersion');
-        NativeObject version = context
-            .newJavaObjectFromConstructor('java.lang.String', null)
-            .invoke(method: "concat", args: ["android "]).invoke(
-                method: "concat", args: [versionId]);
+        NativeObject versionId = context.newJavaObjectFromConstructor('com.example.dna_example.DnaTest', null).invoke(method: 'getDnaVersion').invoke(method: 'getVersion');
+        NativeObject version = context.newJavaObjectFromConstructor('java.lang.String', ["android "]).invoke(method: "concat", args: [versionId]);
+        context.returnVar = version; // 该句可省略
       });
 
       // ObjCContext context = ObjCContext();
